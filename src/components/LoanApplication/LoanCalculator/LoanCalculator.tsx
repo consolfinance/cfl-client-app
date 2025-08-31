6036bf84c707d8e238152646660c450041f84b09
Kumbu <kkamanga16@gmail.com>
2025-08-31 18:20:37 +0300

    [CFL-14] - Refactor LoanCalculator component; remove input fields and retain calculator title

--- src/components/LoanApplication/LoanCalculator/LoanCalculator.tsx
+++ src/components/LoanApplication/LoanCalculator/LoanCalculator.tsx
@@ -14,23 +14,7 @@ const LoanCalculator: FC<LoanCalculatorProps> = ({ values, onChange }) => {
   return (
     <Card padding={0} className={styles.root}>
       <View padding={6} backgroundColor="neutral-faded" className={styles.content}>
-        <input
-          type="number"
-          value={values.amount}
-          onChange={(e) => onChange({ ...values, amount: Number(e.target.value) })}
-        />
-        <input
-          type="number"
-          value={values.term}
-          onChange={(e) => onChange({ ...values, term: Number(e.target.value) })}
-        />
-        <input
-          type="number"
-          value={values.interestRate}
-          onChange={(e) =>
-            onChange({ ...values, interestRate: Number(e.target.value) })
-          }
-        />
+        <Text variant={"featured-2"}>CALCULATOR</Text>
       </View>
     </Card>
   );
